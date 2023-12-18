baac452e0a4adb1fd5d01a7ab7ff77c4dc1a4a7d
fptcsm <gianglamweb@gmail.com>
2023-12-19 02:55:50 +0700

    commit

--- hustcare.sql
+++ hustcare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 11, 2023 at 04:59 AM
+-- Generation Time: Dec 18, 2023 at 08:55 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -60,6 +60,7 @@ INSERT INTO `Attributes` (`id`, `price`, `acreage`, `published`, `hashtag`, `cre
 ('02955fe2-a99d-4610-abdc-bcae9bf2e0f1', '1 triệu/tháng', '20 m2', '05/12/2023', '#844077', '2023-12-05 11:49:21', '2023-12-05 11:49:21'),
 ('72f69ace-5526-47da-bbd2-207fbc30db95', '3 triệu/tháng', '20 m2', '07/06/2023', '#419359', '2023-06-07 14:22:21', '2023-06-07 14:22:21'),
 ('7a0f0e7f-0c64-456a-a64b-ad4dece96c16', '3 triệu/tháng', '20 m2', '07/06/2023', '#708717', '2023-06-07 14:20:25', '2023-06-07 14:20:25'),
+('83b1eddf-d6c1-411f-9b04-58c0f9430373', '10 triệu/tháng', '50 m2', '19/12/2023', '#994393', '2023-12-18 19:03:46', '2023-12-18 19:03:46'),
 ('bc2724ec-f373-43ef-bceb-1f9aaef2541a', '1 triệu/tháng', '20 m2', '05/12/2023', '#549553', '2023-12-05 11:46:51', '2023-12-05 11:46:51');
 
 -- --------------------------------------------------------
@@ -83,7 +84,8 @@ CREATE TABLE `Categories` (
 --
 
 INSERT INTO `Categories` (`id`, `code`, `value`, `header`, `subheader`, `createdAt`, `updatedAt`) VALUES
-(1, 'code123', 'Phòng trọ', 'Phòng trọ, nhà trọ', 'Phòng trọ 2', '2023-06-03 19:23:25', '2023-06-03 19:23:25');
+(1, 'code123', 'Phòng trọ', 'Phòng trọ, nhà trọ', 'Phòng trọ 2', '2023-06-03 19:23:25', '2023-06-03 19:23:25'),
+(2, 'code 1234', 'Share phòng', 'Share phòng', 'Share phòng', '2023-12-19 02:51:25', '2023-12-19 02:51:25');
 
 -- --------------------------------------------------------
 
@@ -136,6 +138,7 @@ INSERT INTO `Images` (`id`, `image`, `createdAt`, `updatedAt`) VALUES
 ('13e11421-68ca-4eb4-a6aa-405bffac1164', '[\"https://res.cloudinary.com/djjv3stcm/image/upload/v1686147546/phongtro123/sn7zthyznafnaefeeohb.png\",\"https://res.cloudinary.com/djjv3stcm/image/upload/v1686147613/phongtro123/z35smxpsan9erfzuajz0.jpg\"]', '2023-06-07 14:20:25', '2023-06-07 14:20:25'),
 ('3ffa01b8-9d4b-4d24-b5a7-c1989f6a8e41', '[\"https://res.cloudinary.com/djjv3stcm/image/upload/v1701776808/phongtro123/iz4ddmnlsdxvt7ola29v.jpg\"]', '2023-12-05 11:46:51', '2023-12-05 11:46:51'),
 ('91cba214-a377-4999-b065-9837befe397b', '[\"https://res.cloudinary.com/djjv3stcm/image/upload/v1686147742/phongtro123/ja9ok5afj7ebdjmoguhn.jpg\"]', '2023-06-07 14:22:21', '2023-06-07 14:22:21'),
+('9fbf3ebf-b945-4a15-ab49-d1fc50705752', '[\"https://res.cloudinary.com/djjv3stcm/image/upload/v1702926223/phongtro123/eaar1ox4obdrjrbl3jjb.jpg\"]', '2023-12-18 19:03:46', '2023-12-18 19:03:46'),
 ('cdb408dd-5def-4dab-b467-317984dc36e6', '[\"https://res.cloudinary.com/djjv3stcm/image/upload/v1701776959/phongtro123/v6d4o8hc1jahvuihhz3b.jpg\"]', '2023-12-05 11:49:21', '2023-12-05 11:49:21');
 
 -- --------------------------------------------------------
@@ -187,6 +190,7 @@ CREATE TABLE `Overviews` (
 
 INSERT INTO `Overviews` (`id`, `code`, `area`, `type`, `target`, `bonus`, `created`, `expire`, `createdAt`, `updatedAt`) VALUES
 ('5806dcbb-9e46-40d6-8fb8-639cc64cb53b', '#549553', 'Phòng trọ Thành phố Hà Nội', 'Phòng trọ', 'Tất cả', 'Tin miễn phí', 'Thứ 3, 18:46 5/12/2023', 'Thứ 3, 18:46 12/12/2023', '2023-12-05 11:46:51', '2023-12-05 11:46:51'),
+('639db6eb-0282-4eae-92ea-c9dd539a30bb', '#994393', 'Phòng trọ Thành phố Hà Nội', 'Phòng trọ', 'Tất cả', 'Tin miễn phí', 'Thứ 3, 2:3 19/12/2023', '30/12/2023', '2023-12-18 19:03:46', '2023-12-18 19:04:53'),
 ('65c5d64e-5dbc-44e2-b5a5-64093ea345ca', '#419359', 'Phòng trọQuận Cầu Giấy', 'Phòng trọ', 'Tất cả', 'Tin miễn phí', 'Thứ 4, 21:22 7/6/2023', '22/06/2023', '2023-06-07 14:22:21', '2023-06-07 15:22:25'),
 ('7e0a8b02-c7c3-4e5f-81cd-44ad93652ad3', '#844077', 'Phòng trọ Thành phố Hà Nội', 'Phòng trọ', 'Tất cả', 'Tin miễn phí', 'Thứ 3, 18:49 5/12/2023', '12/12/2023', '2023-12-05 11:49:21', '2023-12-05 11:50:30'),
 ('ab5ca46a-efdc-4e29-baaf-9cbb525a73a5', '#708717', 'Phòng trọ Thành phố Hà Nội', 'Phòng trọ', 'Tất cả', 'Tin miễn phí', 'Thứ 4, 21:20 7/6/2023', 'Thứ 4, 21:20 14/6/2023', '2023-06-07 14:20:25', '2023-06-07 14:20:25');
@@ -260,6 +264,7 @@ CREATE TABLE `Posts` (
 --
 
 INSERT INTO `Posts` (`id`, `title`, `star`, `labelCode`, `address`, `attributeId`, `priceCode`, `areaCode`, `provinceCode`, `categoryCode`, `description`, `status`, `userId`, `overviewId`, `imageId`, `priceNumber`, `areaNumber`, `createdAt`, `updatedAt`) VALUES
+('156a2341-66fb-443b-bc9c-27685554007e', 'Cho thuê phòng', '3', 'OARN', 'số nhà 1,Phường Giảng Võ,Quận Ba Đình, Thành phố Hà Nội', '83b1eddf-d6c1-411f-9b04-58c0f9430373', NULL, NULL, 'NDOE', 'code123', '\"aa\"', 'checked', '6cfe9b27-63c1-4c89-a8d4-cbd8a35b9f62', '639db6eb-0282-4eae-92ea-c9dd539a30bb', '9fbf3ebf-b945-4a15-ab49-d1fc50705752', 10, 50, '2023-12-18 19:03:46', '2023-12-18 19:04:53'),
 ('60cedf42-39c0-44de-a79e-e857ed4e3c9b', 'Cho thuê phòng trọ', '0', 'OTOI', 'số nhà 1,Phường Cổ Nhuế 1,Quận Bắc Từ Liêm, Thành phố Hà Nội', 'bc2724ec-f373-43ef-bceb-1f9aaef2541a', NULL, NULL, 'NDOE', 'code123', '\"Cho thuê phòng trọ full nội thất\"', 'unChecked', '32e6e1b1-893c-4c12-a4b2-6ea4c78ab8ea', '5806dcbb-9e46-40d6-8fb8-639cc64cb53b', '3ffa01b8-9d4b-4d24-b5a7-c1989f6a8e41', 1, 20, '2023-12-05 11:46:51', '2023-12-05 11:46:51'),
 ('9cf8cb68-e7fd-4614-b3ea-0b3486e6d327', 'Cho thuê phòng trọ giá rẻ', '0', 'OUOH', 'số nhà 51, ngõ 165 đường dương quảng hàm,Phường Quan Hoa,Quận Cầu Giấy, Thành phố Hà Nội', '72f69ace-5526-47da-bbd2-207fbc30db95', NULL, NULL, 'NDOE', 'code123', '\"Cho thuê phòng trọ giá rẻ\"', 'checked', '457fb5e0-ac7c-43ec-ace1-74a4e8e7cd12', '65c5d64e-5dbc-44e2-b5a5-64093ea345ca', '91cba214-a377-4999-b065-9837befe397b', 3, 20, '2023-12-07 14:22:21', '2023-12-07 15:22:25'),
 ('ebff2ccf-83a1-4186-80c9-10fc90bb3dde', 'Cho thuê phòng trọ', '0', 'OARN', 'Số nhà 1,Phường Cống Vị,Quận Ba Đình, Thành phố Hà Nội', '02955fe2-a99d-4610-abdc-bcae9bf2e0f1', NULL, NULL, 'NDOE', 'code123', '\"Cho thuê phòng trọ \"', 'checked', '6cfe9b27-63c1-4c89-a8d4-cbd8a35b9f62', '7e0a8b02-c7c3-4e5f-81cd-44ad93652ad3', 'cdb408dd-5def-4dab-b467-317984dc36e6', 1, 20, '2023-12-05 11:49:21', '2023-12-05 11:50:30');
@@ -368,7 +373,7 @@ CREATE TABLE `Users` (
 --
 
 INSERT INTO `Users` (`id`, `name`, `password`, `phone`, `zalo`, `fbUrl`, `avatar`, `roleCode`, `positionCode`, `createdAt`, `updatedAt`) VALUES
-('32e6e1b1-893c-4c12-a4b2-6ea4c78ab8ea', 'Admin', '$2a$12$5aenhykmyMuVOCIykKiqxufCLNc/00EYF8THCd368SIK4osCPPCzq', '0123456789', NULL, NULL, NULL, 'R1', 'P2', '2023-06-07 14:23:47', '2023-12-05 11:52:02'),
+('32e6e1b1-893c-4c12-a4b2-6ea4c78ab8ea', 'Admin', '$2a$12$5aenhykmyMuVOCIykKiqxufCLNc/00EYF8THCd368SIK4osCPPCzq', '0123456789', NULL, NULL, NULL, 'R1', 'P2', '2023-06-07 14:23:47', '2023-12-18 19:48:54'),
 ('457fb5e0-ac7c-43ec-ace1-74a4e8e7cd12', 'Datist Pham', '$2a$12$KCfQNZMtCaAQqp48XsM8Ve8XHBHDMU7PKqwEysV/9ht7KnbJnCJIi', '0384294882', '01832983912', 'https://facebook.com/123', 'https://res.cloudinary.com/djjv3stcm/image/upload/v1686151132/phongtro123/zjxwuawqc4v4l4i9phgp.png', 'R2', 'P1', '2023-06-07 14:17:34', '2023-06-07 15:22:50'),
 ('55659d58-38f8-40e3-97ea-c8a62ac58ee3', 'Datist Pham', '$2a$12$plVvc52Z7NRfC3q1wP53guiw4lQQGEPZqpfzsE/d6H7bsN/oY0UgW', '0388029483', NULL, NULL, NULL, 'R1', 'P1', '2023-06-03 12:06:39', '2023-12-07 02:57:33'),
 ('6cfe9b27-63c1-4c89-a8d4-cbd8a35b9f62', 'Dat', '$2a$12$iOBUCcfYCZxJRnR.7Qi6m.cpDTqGHKyfy6VI0gPFbjx2K7FqFI1s.', '0123456780', '', '', 'https://res.cloudinary.com/djjv3stcm/image/upload/v1701776973/phongtro123/ropmifxvgol6n6qqh5ms.jpg', 'R2', 'P1', '2023-12-05 11:48:29', '2023-12-05 11:49:34'),
@@ -483,7 +488,7 @@ ALTER TABLE `Areas`
 -- AUTO_INCREMENT for table `Categories`
 --
 ALTER TABLE `Categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `Labels`
